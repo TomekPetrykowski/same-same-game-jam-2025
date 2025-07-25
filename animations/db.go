2e385ba102d1b05ab8b9e1ba63511758f6b79df3
dori <panidora01@gmail.com>
2025-07-25 21:27:17 +0200

    fixed sprites and added to db

--- animations/db.go
+++ animations/db.go
@@ -6,10 +6,22 @@ const (
 	PlayerUpId AnimationId = iota
 	PlayerDownId
 	PlayerIdleId
+	BombheadUpId
+	BombheadDownId
+	BombheadIdleId
+	BirdmanUpId
+	BirdmanDownId
+	BirdmanIdleId
 )
 
 var DB = map[AnimationId]*Animation{
-	PlayerUpId:   NewAnimation(3, 5, 2, 10.0),
-	PlayerDownId: NewAnimation(2, 4, 2, 10.0),
-	PlayerIdleId: NewAnimation(0, 0, 0, 0.0),
+	PlayerUpId:     NewAnimation(3, 5, 2, 10.0),
+	PlayerDownId:   NewAnimation(2, 4, 2, 10.0),
+	PlayerIdleId:   NewAnimation(0, 0, 0, 0.0),
+	BombheadUpId:   NewAnimation(3, 5, 2, 10.0),
+	BombheadDownId: NewAnimation(2, 4, 2, 10.0),
+	BombheadIdleId: NewAnimation(0, 0, 0, 0.0),
+	BirdmanUpId:    NewAnimation(0, 2, 2, 10.0),
+	BirdmanDownId:  NewAnimation(1, 3, 2, 10.0),
+	BirdmanIdleId:  NewAnimation(0, 0, 0, 0.0),
 }
