d6f43f1a0f8a82185bbe5ea4e4c4cd917d8cb5c8
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2025-07-25 13:18:47 +0200

    Clean up and add animation idle

--- entities/player.go
+++ entities/player.go
@@ -1,13 +1,12 @@
 package entities
 
 import (
-	"game/animations"
-	s "game/spritesheets"
+	anim "game/animations"
+	spritesheet "game/spritesheets"
+	"game/utils/images"
 	v "game/utils/math"
-	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type State uint8
@@ -15,81 +14,94 @@ type State uint8
 const (
 	Down State = iota
 	Up
+	Idle
 )
 
 type Player struct {
-	Entity
+	*Entity
 	FacingUp    bool
 	Vel         v.Vec
-	Animations  map[State]*animations.Animation
-	SpriteSheet s.SpriteSheet
+	Animations  map[State]*anim.Animation
+	Spritesheet *spritesheet.Spritesheet
 }
 
-func (p *Player) ActiveAnimation() *animations.Animation {
+func (p *Player) ActiveAnimation() *anim.Animation {
+
+	if dx, dy := p.Vel.Unpack(); dx == 0 && dy == 0 {
+		return p.Animations[Idle]
+	}
 
 	if p.FacingUp {
 		return p.Animations[Up]
 	} else {
 		return p.Animations[Down]
 	}
-	// return nil
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
 	DrawCollider(p.Collider, screen)
-	DrawSprite(screen, p.GetImage(), *p.Entity.Collider.GetPos(), v.Vec{X: -7.5, Y: -20})
+	DrawSprite(screen, p.GetCurrentImage(), *p.Entity.Collider.GetPos(), v.Vec{X: -7.5, Y: -20})
 }
 
 func (p *Player) Update(scene Scene) {
+	// Moved updating animation in releveant method
+	p.ActiveAnimation().Update()
 
-	vel := v.Vec{}
+	p.Vel = v.Vec{}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		vel.X -= 1
+		p.Vel.X -= 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		vel.X += 1
+		p.Vel.X += 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		vel.Y -= 1
+		p.Vel.Y -= 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		vel.Y += 1
+		p.Vel.Y += 1
 	}
-	p.Collider.GetPos().Add(vel)
-	if vel.Y < 0 {
+	p.Collider.GetPos().Add(p.Vel)
+	if p.Vel.Y < 0 {
 		p.FacingUp = true
-	} else if vel.Y > 0 {
+	} else if p.Vel.Y > 0 {
 		p.FacingUp = false
 	}
 
-	for _, o := range (*scene.GetObjects())["staticObjects"] {
+	for _, o := range (*scene.GetObjects())[StaticsObjectId] {
 		p.Collider.CollideAndSlide(*o.GetCollider())
 	}
 
 }
 
-func (p *Player) GetImage() *ebiten.Image {
-
-	p.ActiveAnimation().Update()
-
-	return (p.Entity.Sprite.Img.SubImage(p.SpriteSheet.Rect(p.ActiveAnimation().Frame()))).(*ebiten.Image)
-
+func (p *Player) GetCurrentImage() *ebiten.Image {
+	return images.SubImage(
+		p.Entity.Sprite.Img,
+		p.Spritesheet.Rect(
+			p.ActiveAnimation().Frame(),
+		),
+	)
 }
 
-func NewPlayer(x, y float64) *Player {
-	sprite, _, err := ebitenutil.NewImageFromFile("assets/player.png")
-	if err != nil {
-		// handle error
-		log.Fatal(err)
+func NewPlayer(initX, initY float64) *Player {
+
+	playerImg := images.LoadImage(
+		"assets/player.png",
+		"Error while loading player image.",
+		&images.DefaultPlaceholder,
+	)
+
+	animations := map[State]*anim.Animation{
+		Up:   anim.DB[anim.PlayerUpId],
+		Down: anim.DB[anim.PlayerDownId],
+		Idle: anim.DB[anim.PlayerIdleId],
 	}
-	animations := map[State]*animations.Animation{
-		Up:   animations.NewAnimation(3, 5, 2, 20.0),
-		Down: animations.NewAnimation(2, 4, 2, 20.0),
+
+	sprite := Sprite{Img: playerImg}
+	ent := NewEntity(NewCircle(initX, initY, 5), &sprite)
+
+	return &Player{
+		Entity:      ent,
+		Animations:  animations,
+		Spritesheet: spritesheet.DB[spritesheet.PlayerSpritesheedId],
 	}
-	ent := *NewEntity(NewCircle(x, y, 5))
-	// img := ebiten.NewImage(20, 20)
-	// img.Fill(color.White)
-	// ent.Sprite.Img = img
-	ent.Sprite = &Sprite{Img: sprite}
-	return &Player{Entity: ent, Animations: animations, SpriteSheet: *s.NewSpriteSheet(2, 3, 15, 26)}
 }
